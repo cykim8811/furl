f187f83f0cd255fd4a8a8cdd047c3fc6a9de6e80
cykim8811 <cykim8811@snu.ac.kr>
2022-09-13 05:53:49 +0000

    Archive 220913.1452 - Assault extrinsic reward training

--- main.py
+++ main.py
@@ -5,50 +5,33 @@ import furl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchinfo
 
 from models.cartpole import CartpoleModel
 from models.breakout import BreakoutModel, BreakoutICMModel
+from models.assault import AssaultICMModel
+from models.gym import AtariICMModel, AtariModel
 
 import wandb
 
-from stable_baselines3.common.atari_wrappers import ClipRewardEnv, EpisodicLifeEnv, FireResetEnv, MaxAndSkipEnv, NoopResetEnv
-
-def make_breakout_state():
-    env = gym.make("BreakoutNoFrameskip-v4")
-    env = gym.wrappers.RecordEpisodeStatistics(env)
-    env = NoopResetEnv(env, noop_max=30)
-    env = MaxAndSkipEnv(env, skip=4)
-    env = EpisodicLifeEnv(env)
-    if "FIRE" in env.unwrapped.get_action_meanings():
-        env = FireResetEnv(env)
-    env = ClipRewardEnv(env)
-    env = gym.wrappers.ResizeObservation(env, (84, 84))
-    env = gym.wrappers.GrayScaleObservation(env)
-    env = gym.wrappers.FrameStack(env, 4)
-    return furl.gym.GymState(env)
-
-def make_cartpole_state():
-    env = gym.make("CartPole-v1")
-    return furl.gym.GymState(env)
-
 if __name__ == "__main__":
-    model = BreakoutICMModel().cuda()
+    model = AtariModel(7).cuda()
     strategy_param = {
         'gamma': 0.99,
         'normalize_advantage': True,
         'use_gae': True,
         'gae_lambda': 0.95,
-        'clip_eps': 0.2,
+        'clip_eps': 0.1,
     }
     param={
         'lr': 0.001,
-        'update_interval': 128,
+        'update_interval': 256,
         'epochs': 4,
-        'num_processes': 3,
-        'wandb_project': 'breakout-icm',
-        'forward_loss_coef': 10.0,
+        'num_processes': 2,
+        'wandb_project': 'assault-icm',
+        'forward_loss_coef': 3.0,
         'inverse_loss_coef': 0.3,
     }
-    strategy = furl.algorithms.PPOICMStrategy(**strategy_param)
+    strategy = furl.algorithms.PPOStrategy(**strategy_param)
     trainer = furl.Trainer(param, strategy)
-    trainer.fit(model, make_breakout_state, total_steps=None)
+    trainer.fit(model, furl.gym.make_atari_state("AssaultNoFrameskip-v4"), total_steps=None)
---
+++ models/gym.py
@@ -0,0 +1,103 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+class AtariModel(nn.Module):
+    def __init__(self, num_actions):
+        super().__init__()
+        self.num_actions = num_actions
+        self.conv = nn.Sequential(
+            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
+            nn.ReLU(),
+        )
+        self.actor = layer_init(nn.Linear(3136, num_actions), std=0.01)
+        self.critic = layer_init(nn.Linear(3136, 1), std=1)
+
+    def forward(self, x):
+        batched = x.dim() == 4
+        if not batched: x = x.unsqueeze(0)
+        x = x.float() / 255
+        x1 = self.conv(x)
+        x2 = self.conv(x)
+        x1 = x1.view(x1.size(0), -1)
+        if not batched: x1 = x1.squeeze(0)
+        x2 = x2.view(x2.size(0), -1)
+        if not batched: x2 = x2.squeeze(0)
+        return self.actor(x1), self.critic(x2)
+
+
+class AtariICMModel(nn.Module):
+    def __init__(self, num_actions):
+        super().__init__()
+        self.num_actions = num_actions
+        self.conv = nn.Sequential(
+            layer_init(nn.Conv2d(4, 32, 8, stride=4)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
+            nn.ReLU(),
+        )
+        self.actor = layer_init(nn.Linear(3136, num_actions), std=0.01)
+        self.critic = layer_init(nn.Linear(3136, 1), std=1)
+
+        self.icm_feature = nn.Sequential(
+            layer_init(nn.Linear(3136, 512)),
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),
+        )
+        self.icm_inverse = layer_init(nn.Linear(1024, num_actions), std=0.01)
+        self.icm_forward = nn.Sequential(
+            layer_init(nn.Linear(512 + num_actions, 512)),
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),
+        )
+
+
+    def forward(self, x):
+        batched = x.dim() == 4
+        if not batched: x = x.unsqueeze(0)
+        x = x.float() / 255
+        x1 = self.conv(x)
+        x2 = self.conv(x)
+        x1 = x1.view(x1.size(0), -1)
+        if not batched: x1 = x1.squeeze(0)
+        x2 = x2.view(x2.size(0), -1)
+        if not batched: x2 = x2.squeeze(0)
+        return self.actor(x1), self.critic(x2)
+
+    def icm(self, s0, a, s1):
+        s0 = s0.float() / 255
+        s1 = s1.float() / 255
+        s0 = self.conv(s0)
+        s1 = self.conv(s1)
+        s0 = s0.view(s0.size(0), -1)
+        s1 = s1.view(s1.size(0), -1)
+        s0 = self.icm_feature(s0)
+        s1 = self.icm_feature(s1)
+        a_onehot = F.one_hot(a, self.num_actions).float()
+        s1_pred = self.icm_forward(torch.cat([s0, a_onehot], dim=1))
+        action_pred = self.icm_inverse(torch.cat([s0, s1], dim=1))
+
+        pred_error = (s1_pred - s1).pow(2).mean(dim=1)
+        action_loss = F.cross_entropy(action_pred, a)
+        return pred_error, action_loss
